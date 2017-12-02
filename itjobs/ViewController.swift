151497289e718e9ab7ee38e30d6ec1c7f79c0a58
Piotr Smajek <piotr.smajek@miquido.com>
2017-12-02 15:17:47 +0100

    init commit

---
+++ itjobs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  itjobs
+//
+//  Created by Piotrek on 02.12.2017.
+//  Copyright © 2017 Piotr Smajek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
